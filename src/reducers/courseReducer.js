6cdde6eb3b7f7bd051ca7bd6e9a37acdd329fb12
Anthonie Lance <anthonielance@users.noreply.github.com>
2017-06-06 11:34:08 -0400

    Async Writes in Redux

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,9 +1,22 @@
 import * as types from "../actions/actionTypes";
+import initialSate from "./initialState";
 
-export default function courseReducer(state = [], action) {
+export default function courseReducer(state = initialSate.courses, action) {
   switch (action.type) {
-    case types.LOAD_COURSE_SUCCESS:
+    case types.LOAD_COURSES_SUCCESS:
       return action.courses;
+
+    case types.CREATE_COURSE_SUCCESS:
+      return [
+        ...state,
+        Object.assign({}, action.course)
+      ];
+
+    case types.UPDATE_COURSE_SUCCESS:
+      return [
+        ...state.filter(course => course.id !== action.course.id),
+        Object.assign({}, action.course)
+      ];
 
     default:
       return state;
